6a4ce7ee88dc00cc0b01aa581130542bc8b49fee
iammrknight <matthewryanknight@gmail.com>
2020-07-01 22:02:24 -0400

    More items!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
     {
       title: 'Augmented Torso Frame',
       lore: 'Protects the most important parts, with some unexpected magical perks.',
-      img: 'assets/img/power_leather.png',
+      img: 'assets/img/power_leather.jpg',
       focus: false,
       description: [
         '+2 Leather Armor',
@@ -22,7 +22,7 @@ export class AppComponent {
     {
       title: 'Full Auto Crossbow',
       lore: 'The steam-smiths were trying to make a nail gun, instead they made a war crime.',
-      img: 'assets/img/crossbow.png',
+      img: 'assets/img/crossbow.jpg',
       focus: false,
       description: [
         'A +2 Heavy Auto Crossbow',
@@ -34,7 +34,7 @@ export class AppComponent {
     {
       title: 'Steamsmithing Goggles',
       lore: 'An essential piece of safety gear for delicate crafting work, only some of the attachments are cosmetic.',
-      img: 'assets/img/steampunk_goggles.png',
+      img: 'assets/img/steampunk_goggles.jpg',
       focus: false,
       description: [
         'Adds +2 to Wisdom',
@@ -44,7 +44,7 @@ export class AppComponent {
     {
       title: 'Staff of Forging',
       lore: 'Smithing tool reserved for elite forgemancers and wizards who like to start fires.',
-      img: 'assets/img/fire_staff.png',
+      img: 'assets/img/fire_staff.jpg',
       focus: false,
       description: [
         'A +2 Arcane Spellcasting Focus',
@@ -56,7 +56,7 @@ export class AppComponent {
     {
       title: 'Forgemaster Gauntlet',
       lore: 'For when handling with care isn\'t an option.',
-      img: 'assets/img/gauntlet.png',
+      img: 'assets/img/gauntlet.jpg',
       focus: false,
       description: [
         'While wearing this gauntlet you have +2 AC and resistance to fire damage',
@@ -73,7 +73,7 @@ export class AppComponent {
     {
       title: 'Twitter Influencer Cloak',
       lore: 'Created for the Druids operating the Twitter network.',
-      img: 'assets/img/animal_cloak.png',
+      img: 'assets/img/animal_cloak.jpg',
       focus: false,
       description: [
         'Adds +2 to Charisma',
@@ -89,11 +89,11 @@ export class AppComponent {
     {
       title: 'Onyx Lute',
       lore: 'Custom-cursed by the witches of Deathmyst Peninsula.',
-      img: 'assets/img/onyx_lute.png',
+      img: 'assets/img/onyx_lute.jpg',
       focus: false,
       description: [
         'A +2 Bardic Focus',
-        'If you are proficient in lutes and play this instrument as part of a Performance check any roles less than your Charisma score are counted as being equal to your Charisma score.',
+        'If you are proficient in lutes and play this instrument as part of a Performance check any rolls less than your Charisma score are counted as being equal to your Charisma score.',
         'Once per day the Onyx Lute can cast Immolation.  This does not expend a spell slot.  After using this feature roll a d20, on a 1 the Onyx Lute must be re-cursed by a powerful witch before this feature can be used again.'
       ],
       links: [
@@ -106,7 +106,7 @@ export class AppComponent {
     {
       title: 'Staff of Quenching',
       lore: 'Smithing tool reserved for elite forgemancers and wizards who like to freeze things.',
-      img: 'assets/img/ice_staff.png',
+      img: 'assets/img/ice_staff.jpg',
       focus: false,
       description: [
         'A +2 Arcane Spellcasting Focus',
@@ -118,7 +118,7 @@ export class AppComponent {
     {
       title: 'Evil Resistant Armor',
       lore: 'Top notch armor for combatting evil creatures.',
-      img: 'assets/img/dark_armor.png',
+      img: 'assets/img/dark_armor.jpg',
       focus: false,
       description: [
         '+2 Studded Leather Armor',
@@ -130,7 +130,7 @@ export class AppComponent {
     {
       title: 'Belt of Dwarvenkind',
       lore: 'A wonderous item for true dwarf friends',
-      img: 'assets/img/dwarven_belt.png',
+      img: 'assets/img/dwarven_belt.jpg',
       focus: false,
       description: [
         'While wearing this belt your Constitution score increases by 2, to a maximum of 20, and you have advantage on Charisma (Persuasion) checks made to interact with dwarves.',
@@ -144,7 +144,7 @@ export class AppComponent {
     {
       title: 'Swamphunter Bow',
       lore: 'Repossessed from a witch hunter in Deathmyst Peninsula.',
-      img: 'assets/img/hunter_bow.png',
+      img: 'assets/img/hunter_bow.jpg',
       focus: false,
       description: [
         'A +2 Magical Longbow',
@@ -166,12 +166,12 @@ export class AppComponent {
     {
       title: 'Clockwork Violin',
       lore: 'An inexplicable hodgepodge of art and science, functions as a clock, violin, and weapon of mass destruction.',
-      img: 'assets/img/violin.png',
+      img: 'assets/img/violin.jpg',
       focus: false,
       description: [
         'A +2 Bardic Focus',
         'Can be used to tell time, which is cool.',
-        'If you are proficient in violin and play this instrument as part of a Performance check any roles less than your Charisma score are counted as being equal to your Charisma score.',
+        'If you are proficient in violin and play this instrument as part of a Performance check any rolls less than your Charisma score are counted as being equal to your Charisma score.',
         'Once per day the Clockwork Violin can cast Destructive Wave.  This does not expend a spell slot.  After using this feature roll a d20, on a 1 the Clockwork Violin must be repaired by a steamsmith before this feature can be used again.'
       ],
       links: [
